f37d5ba61dbb01443d9b6cb81d9c192dd71a0133
Byron Theobald <byrontheobald@gmail.com>
2020-11-23 11:46:59 +0000

    drive m2 through apu dummy

--- rtl/cv32e40n_apu_dummy.sv
+++ rtl/cv32e40n_apu_dummy.sv
@@ -14,7 +14,17 @@ module cv32e40n_apu_dummy import cv32e40p_apu_core_pkg::*;
     output logic [APU_NUSFLAGS_CPU-1:0]     apu_flags_o,
     output logic                            apu_gnt_o,
     
-    output logic                            mem_master_sel);
+    output logic                            mem_master_sel,
+
+    // Data memory interface
+    output logic                            data_req_o,
+    input  logic                            data_gnt_i,
+    input  logic                            data_rvalid_i,
+    output logic                            data_we_o,
+    output logic [3:0]                      data_be_o,
+    output logic [31:0]                     data_addr_o,
+    output logic [31:0]                     data_wdata_o,
+    input  logic [31:0]                     data_rdata_i );
 
     assign apu_result_o = 'd0;
     assign apu_flags_o  = 'd0;
@@ -28,6 +38,12 @@ module cv32e40n_apu_dummy import cv32e40p_apu_core_pkg::*;
         else
             current_s <= next_s;
     end
+
+    assign data_req_o = 1'b0;
+    assign data_we_o  = 1'b0;
+    assign data_be_o  = 4'b0;
+    assign data_addr_0 = 31'd0;
+    assign data_wdata_0 = 31'd0;
 
     always_comb begin
         apu_gnt_o = '0;
